c03d63585c93304680b4d340cfc9c3d525d2466a
Bunmi Mise <bunmi_mise@yahoo.com>
2018-08-14 17:24:24 -1000

    feat: a Hero class was created and player object was instantiated

--- js/app.js
+++ js/app.js
@@ -21,11 +21,20 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+class Hero { 
+    constructor () {
+      this.x = 0;
+      this.y = 0;
+      this.sprite =  'images/char-boy.png';
+       }
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+    render() {
+       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
+    }
+}
+const player = new Hero();
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
